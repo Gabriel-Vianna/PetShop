a707094623ffd5d6b449b25912411c6fc53acb1a
Gabriel Vianna <gabriel1_vianna@hotmail.com>
2022-04-07 02:04:56 -0300

    Add firestore database

--- app/src/main/java/br/edu/infnet/petshop/ui/perfil/PerfilActivity.kt
+++ app/src/main/java/br/edu/infnet/petshop/ui/perfil/PerfilActivity.kt
@@ -13,6 +13,8 @@ import br.edu.infnet.petshop.ui.listaservicos.ServicoItemAdapter
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_perfil.recyclerViewHistory
 
 class PerfilActivity : AppCompatActivity() {
@@ -23,6 +25,7 @@ class PerfilActivity : AppCompatActivity() {
     private final var TAG = "HomeActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val db = Firebase.firestore
         super.onCreate(savedInstanceState)
         binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -30,6 +33,45 @@ class PerfilActivity : AppCompatActivity() {
         MobileAds.initialize(this) {}
 
         var adRequest = AdRequest.Builder().build()
+
+        val arrayList = ArrayList<ServicoModel>()
+        db.collection("agendamentos")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    var imagem: Int = R.drawable.banho_logo
+                    if(document.data["title"].toString() == "Tosa") {
+                        imagem = R.drawable.tosa_logo
+                    }
+                    if(document.data["title"].toString() == "Vacina") {
+                        imagem = R.drawable.vacina_logo
+                    }
+                    if(document.data["title"].toString() == "Consulta") {
+                        imagem = R.drawable.consulta_logo
+                    }
+                    if(document.data["title"].toString() == "Taxi Dog") {
+                        imagem = R.drawable.taxi
+                    }
+                    var servico = ServicoModel(document.data["title"].toString(),
+                        document.data["attendance"].toString(),
+                        document.data["days"].toString(),
+                        document.data["hour"].toString(),
+                        document.data["value"].toString(),
+                        imagem)
+
+                    arrayList.add(servico)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
+        recyclerViewHistory.adapter = servicoItemAdapter
+        recyclerViewHistory.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = PerfilServicoItemAdapter(arrayList, this.context)
+        }
 
         InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
             override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -40,6 +82,7 @@ class PerfilActivity : AppCompatActivity() {
             override fun onAdLoaded(interstitialAd: InterstitialAd) {
                 Log.d(TAG, "Ad was loaded.")
                 mInterstitialAd = interstitialAd
+                startAdSense()
             }
         })
 
@@ -59,42 +102,10 @@ class PerfilActivity : AppCompatActivity() {
 
         }
 
-        val arrayList = ArrayList<ServicoModel>()
 
-        arrayList.add(
-            ServicoModel(
-                "Banho", "Serviço realizado em:", "01/12/2021", "14:00h", "R$50,00",
-                R.drawable.banho_logo
-            )
-        )
-        arrayList.add(
-            ServicoModel(
-                "Tosa", "Serviço realizado em:", "01/12/2021", "13:00h", "R$70,00",
-                R.drawable.tosa_logo
-            )
-        )
-        arrayList.add(
-            ServicoModel(
-                "Tosa", "Serviço realizado em:", "01/12/2021", "13:00h", "R$70,00",
-                R.drawable.tosa_logo
-            )
-        )
-        arrayList.add(
-            ServicoModel(
-                "Tosa", "Serviço realizado em:", "01/12/2021", "13:00h", "R$70,00",
-                R.drawable.tosa_logo
-            )
-        )
-
-
-        recyclerViewHistory.adapter = servicoItemAdapter
-        recyclerViewHistory.apply {
-            layoutManager = LinearLayoutManager(this.context)
-            adapter = PerfilServicoItemAdapter(arrayList, this.context)
-        }
     }
 
-    fun startAdSense(view: View) {
+    fun startAdSense() {
         if (mInterstitialAd != null) {
             mInterstitialAd?.show(this)
         } else {
